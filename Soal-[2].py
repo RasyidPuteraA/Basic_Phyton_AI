4da44aefa4ef71e78d213741ef370dfa9ec42237
Your Name <rasyid_puteraagung@yahoo.co.id>
2022-08-04 13:07:59 +0700

    4-8-22

---
+++ Soal-[2].py
@@ -0,0 +1,9 @@
+import math
+
+x = math.pi
+
+value = int(input('Masukkan nilai jari - jari lingkaran : '))
+
+result = x*value**2
+
+print(f"Luas lingkaran dengan jari-jari {value} cm adalah {result} cm\u00b2 .")
