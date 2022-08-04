bbe2fef2ccab9c002e147b45075c6b0da2eeb401
Your Name <rasyid_puteraagung@yahoo.co.id>
2022-08-04 12:37:30 +0700

    4-8-22

---
+++ Soal-[1].py
@@ -0,0 +1,6 @@
+
+name = input('Masukkan nama : ')
+age = int(input('Masukkan Umur : '))
+height = float(input('Masukkan Tinggi Badan : '))
+
+print(f'Nama saya {name}, umur saya {age} tahun dan tinggi saya {height} cm.')
