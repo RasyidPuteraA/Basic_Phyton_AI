8c0b7322db0fd262615ff9eb4bea726d1fadbe27
Your Name <rasyid_puteraagung@yahoo.co.id>
2022-08-07 15:09:44 +0700

    7-8-22

---
+++ Pertemuan 4/Quiz4.py
@@ -0,0 +1,10 @@
+mydict = {
+    'Name' : 'Zara',
+    'Age' : 7,
+    'Class' : 'Frist'
+}
+mydict['Age'] = 8
+mydict['School'] = 'DPS School'
+
+print("dict['Age'] : ",mydict['Age']) 
+print("dict['School'] : ",mydict['School']) 
---
+++ Pertemuan 4/Quiz5.py
@@ -0,0 +1,8 @@
+def myfunction():
+    x = 10
+    print('Value inside function',x)
+    
+x = 20
+
+myfunction()
+print('Value outside function :',x)
