6a4813608371f5167e47c603d520f9b3eda269fa
Your Name <rasyid_puteraagung@yahoo.co.id>
2022-08-10 21:19:34 +0700

    10-8-22

---
+++ Pertemuan 4/BasicPhyton-Tugas2.py
@@ -0,0 +1,34 @@
+print('Selamat Datang!')
+while True:
+    print('---Menu---\n1.Daftar Kontak\n2.Tambah Kontak\n3.Keluar\n\n')
+    x=int(input('Pilih Menu :'))
+    daftar_contact = open('data.txt','a')
+    read_data = open('data.txt','r')
+    data = read_data.read()
+    if x == 1:
+        print('\nDaftar Kontak :')
+        print(data)
+        continue
+    elif x == 2:
+        while True: 
+            print('Masukkan kontak baru :')
+            Nama = input('Nama : ')
+            number = input('No Telepon : ')
+            lbl = (f'Nama : {Nama} \nNo Telpon : {number}\n\n')
+            daftar_contact.write(lbl)
+            print('Kontak Baru Telah Terdaftar\n')
+            read_data.read()
+            break
+        continue
+    elif x == 3:
+        print('Program selesai, sampai jumpa lagi')
+        read_data.close()
+        break
+    else :
+        print('Menu tidak tersedia')
+        continue    
+    
+
+
+
+        
